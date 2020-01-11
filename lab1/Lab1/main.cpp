f139aa28ae33ef0f50ece00c0ca4b5013ebe1b04
miceorbirds <45165477+miceorbirds@users.noreply.github.com>
2020-01-11 20:06:49 +0300

    delete useless comments

--- lab1/Lab1/main.cpp
+++ lab1/Lab1/main.cpp
@@ -20,11 +20,10 @@ int main()
 
 	quicksort(a, a + size, [](const auto& a, const auto& b) { return a < b; });
 
-	// DEBUG
+
 	for (int i = 0; i < size; i++)
 	{
 		cout << a[i] << " ";
 	}
 	cout << endl;
-	// END DEBUG
 }
